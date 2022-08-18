06af9cedc3db64afe4c4127aac7cc336348fe43c
mecca <fnjakai@outlook.com>
2022-08-18 13:09:51 +0300

    animations are possible now :)

--- 0x07-wordle_clone/src/components/Wordle.js
+++ 0x07-wordle_clone/src/components/Wordle.js
@@ -1,26 +1,25 @@
 import React, { useEffect } from 'react'
 import useWordle from '../hooks/useWordle'
-import Grid from './Grid';
+import Grid from './Grid'
 
 const Wordle = ({solution}) => {
     const { currentGuess, handleKeyup, guesses, isCorrect, turn } = useWordle(solution);
 
     useEffect(() => {
       window.addEventListener('keyup', handleKeyup);
-      return window.removeEventListener('keyup', handleKeyup);
+      return () => window.removeEventListener('keyup', handleKeyup);
     }, [handleKeyup]);
 
     useEffect(() => {
       console.log(guesses, turn, isCorrect);
-    }, [guesses, turn, isCorrect]);
-
+    }, [handleKeyup]); 
+    
     return (
       <div>
           <div>solution: {solution}</div>
           <div>current guess: {currentGuess}</div>
           <Grid currentGuess={currentGuess} guesses={guesses} turn={turn}/>
-      </div>
-    
+      </div>  
   );
 }
 
