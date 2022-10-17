142e7aaa0b8ff9a3e85aa662bf8b396527c5109f
brk-a <fnjakai@outlook.com>
2022-10-17 12:34:10 +0000

    `CardDeals`: complete

--- 0x09-multi_page_website/src/components/CardDeals.jsx
+++ 0x09-multi_page_website/src/components/CardDeals.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+// import React from 'react'
+import styles, {layout} from "../style"
+import { card } from "../assets"
+import Button from "./Button"
 
-const CardDeals = () => {
-  return (
-    <div>CardDeals</div>
-  )
-}
+const CardDeals = () => (
+    <section className={layout.section}>
+
+      <div className={layout.sectionInfo}>
+        <h2 className={styles.heading2}>
+          Find a better card deal <br className='sm:block hidden'/>
+          quickly and conveniently.
+        </h2>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+          We have partnerships that bring innovation,
+          efficiency and convenience to you. You can now work and play
+          from anywhere, at any time, with anyone, however you like.
+        </p>
+        <Button styles={`mt-10`}/>
+      </div>
+
+      <div className={layout.sectionImg}>
+        <img src={card} alt="card-deals" className='w-[100%] h-[100%]'/>
+      </div>
+    </section>
+)
 
 export default CardDeals
