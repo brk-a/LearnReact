c51b2f677b8b35671650f035e71e7b7accd35401
brk-a <fnjakai@outlook.com>
2022-10-18 03:13:31 +0000

    minor fixes in `hero.jsx`

--- 0x09-multi_page_website/src/components/Hero.jsx
+++ 0x09-multi_page_website/src/components/Hero.jsx
@@ -6,7 +6,7 @@ import GetStarted from '../components/GetStarted'
 const Hero = () => {
   return (
     <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
-      <div className={`flex-1 md:flex-row flex-col xl:px-0 sm:px16 px-6`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
         
         <div className='flex flex-row items-center py-[6px] px-4 bg-discounted-gradient rounded-[10px] mb-2'>
           <img src={discount} alt="discount" className="w-[32px h-[32px]"/>
@@ -41,7 +41,7 @@ const Hero = () => {
         
       </div>
 
-      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+      <div  className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
         <img src={robot} alt="robot" className='w-[100%] h-[100%] relative z-[5]'/>
         <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient"/>
         <div className="absolute z-[1] w-[80%] h-[80%] rounded-full bottom-40 white__gradient"/>
