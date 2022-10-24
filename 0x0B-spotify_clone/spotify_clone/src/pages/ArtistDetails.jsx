42566efb064941c1e1afebe1f313c928e43d34f1
brk-a <fnjakai@outlook.com>
2022-10-24 09:27:49 +0000

    `ArtistDetails` part 2: done

--- 0x0B-spotify_clone/spotify_clone/src/pages/ArtistDetails.jsx
+++ 0x0B-spotify_clone/spotify_clone/src/pages/ArtistDetails.jsx
@@ -1,29 +1,33 @@
-import { useParams } from "react-router-dom";
-import { useSelector} from "react-redux";
-import { DetailsHeader, Error, Loader, RelatedSongs } from "../components";
-import { useGetArtistDetailsQuery } from "../redux/services/shazamCore";
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { DetailsHeader, Error, Loader, RelatedSongs } from '../components';
+
+import { useGetArtistDetailsQuery } from '../redux/services/shazamCore';
 
 const ArtistDetails = () => {
-   
-    const {id: artistId} = useParams()
-    const {activeSong, isPlaying} = useSelector((state) => (
-        state.player
-    ))
-    const {data: artistData, isFetching: isFetchingArtistDetails, error} = useGetArtistDetailsQuery(artistId)
-    
-    
-    if (isFetchingArtistDetails) {
-      <Loader title={`Fetching artist details...`} />
-    }
+  const { id: artistId } = useParams();
+  const { activeSong, isPlaying } = useSelector((state) => state.player);
+  const { data: artistData, isFetching: isFetchingArtistDetails, error } = useGetArtistDetailsQuery(artistId);
 
-    if (error) return <Error />
+  if (isFetchingArtistDetails) return <Loader title="Fetching artist details..." />;
 
-    return(
-      <div className="flex flex-col">
-        <DetailsHeader artistId={artistId} artistData={artistData}/>
-        <RelatedSongs data={Object.values(artistData?.songs)} artistId={artistId} isPlaying={isPlaying} activeSong={activeSong} />
-      </div>
-    )
-}
+  if (error) return <Error />;
+
+  return (
+    <div className="flex flex-col">
+      <DetailsHeader
+        artistId={artistId}
+        artistData={artistData}
+      />
+
+      <RelatedSongs
+        data={Object.values(artistData?.songs)}
+        artistId={artistId}
+        isPlaying={isPlaying}
+        activeSong={activeSong}
+      />
+    </div>
+  );
+};
 
 export default ArtistDetails;
