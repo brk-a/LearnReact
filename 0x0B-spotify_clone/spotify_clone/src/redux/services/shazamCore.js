a2f0a056ff8ab2feaf29d543df26b89e10aed64b
brk-a <fnjakai@outlook.com>
2022-10-25 10:57:35 +0000

    `AroundYou` is still not working. Error 422

--- 0x0B-spotify_clone/spotify_clone/src/redux/services/shazamCore.js
+++ 0x0B-spotify_clone/spotify_clone/src/redux/services/shazamCore.js
@@ -27,7 +27,7 @@ export const shazamCoreApi = createApi({
         getSongDetails: builder.query({query: ({songid}) => (`/tracks/details?track_id=${songid}`)}),
         getSongRelated: builder.query({query: ({songid}) => (`/tracks/related?track_id=${songid}`)}),
         getArtistDetails: builder.query({query: (artistId) => (`/artists/details?artist_id=${artistId}`)}),
-        getSongByCountry: builder.query({query: (countryCode) => (`/charts/country?.country_code=${countryCode}`)}),
+        getSongsByCountry: builder.query({ query: (countryCode) => `/charts/country?country_code=${countryCode}` }),
     }),
 })
 
@@ -36,5 +36,5 @@ export const {
     useGetSongDetailsQuery,
     useGetSongRelatedQuery,
     useGetArtistDetailsQuery,
-    useGetSongByCountryQuery,
+    useGetSongsByCountryQuery,
 } = shazamCoreApi
