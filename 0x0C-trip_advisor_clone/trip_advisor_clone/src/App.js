06ead44b8ba4ce5af6cacae821a3dc5fab7dc96d
brk-a <fnjakai@outlook.com>
2022-10-26 16:40:33 +0000

    styles for `Header`,`List`, `Map` and `PlaceDetails`. `Header` , part 1

--- 0x0C-trip_advisor_clone/trip_advisor_clone/src/App.js
+++ 0x0C-trip_advisor_clone/trip_advisor_clone/src/App.js
@@ -10,14 +10,19 @@ const App = () => {
   return(
     <>
       <CssBaseline/>
+
       <Header/>
+
       <Grid container spacing={3} style={{width: '100%'}}>
+        
         <Grid item xs={12} md={4}>
           <List />
         </Grid>
+        
         <Grid item xs={12} md={4}>
           <Map />
         </Grid>
+        
       </Grid>
     </>
   )
--- 0x0C-trip_advisor_clone/trip_advisor_clone/src/components/Header/Header.jsx
+++ 0x0C-trip_advisor_clone/trip_advisor_clone/src/components/Header/Header.jsx
@@ -1,8 +1,35 @@
 import React from 'react'
+import { Autocomplete } from '@react-google-maps/api'
+import {AppBar, Toolbar, Typography, InputBase, Box} from '@material-ui/core'
+import SearchIcon from '@material-ui/icons/Search'
+import useStyles from './HeaderStyles'
+ 
 
 const Header = () => {
+
+  const classes = useStyles()
   return (
-    <div>Header</div>
+    <AppBar position='static'>
+      <Toolbar className={classes.toolbar}>
+        <Typography variant='h5' className={classes.title}>
+          Triq Advisor
+        </Typography>
+        {/* get no truth, then don't go */}
+        <Box display='flex'>
+          <Typography variant='h6' className={classes.title}>
+            Explore
+          </Typography>
+          <Autocomplete>
+            <div  className={classes.search}>
+              <div className={classes.searchIcon}>
+                <SearchIcon />
+              </div>
+              <InputBase placeholder='Search...' classes={{root: classes.inputRoot, input: classes.inputInput}}/>
+            </div>
+          </Autocomplete>         
+        </Box>
+      </Toolbar>
+    </AppBar>
   )
 }
 
