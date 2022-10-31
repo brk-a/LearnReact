8f4eda3d25c7e3dd0bb787245b1ce8a56f3bc6aa
brk-a <fnjakai@outlook.com>
2022-10-31 10:14:53 +0000

    half-built `Navbar`

--- 0x0D-nf_crypto/nf_crypto/src/components/Navbar.jsx
+++ 0x0D-nf_crypto/nf_crypto/src/components/Navbar.jsx
@@ -3,6 +3,7 @@ import { Button, Menu, Typography, Avatar } from 'antd'
 import {Link} from 'react-router-dom'
 import { HomeOutlined, MoneyCollectOutlined, BulbOutlined, FundOutlined, MenuOutlined } from '@ant-design/icons'
 import {icon} from '../images/cryptocurrency.png'
+import MenuItem from 'antd/lib/menu/MenuItem'
 
 const Navbar = () => {
   return (
@@ -12,6 +13,22 @@ const Navbar = () => {
             <Typography.Title level={2} className='logo'>
                 <Link to='/'>NF Crypto</Link>
             </Typography.Title>
+            <div>
+              <Menu theme='dark'>
+                <Menu.Item icon={<HomeOutlined/>} key='home'>
+                  <Link to='/'>Home</Link>
+                </Menu.Item>
+                <Menu.Item icon={<FundOutlined/>} key='cryptocurrencies'>
+                  <Link to='/cryptocurrencies'>Cryptocurrencies</Link>
+                </Menu.Item>
+                <Menu.Item icon={<MoneyCollectOutlined/>} key='exchanges'>
+                  <Link to='/exchanges'>Exchanges</Link>
+                </Menu.Item>
+                <Menu.Item icon={<BulbOutlined/>} key='news'>
+                  <Link to='/news'>News</Link>
+                </Menu.Item>
+              </Menu>
+            </div>
             {/* <Button className='menu-control-container'></Button> */}
         </div>
     </div>
