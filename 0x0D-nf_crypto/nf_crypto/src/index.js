add4d8b5b84694819e5e51c3d01f85b03313eb45
brk-a <fnjakai@outlook.com>
2022-11-04 11:38:49 +0000

    redux: initial set-up

--- 0x0D-nf_crypto/nf_crypto/src/index.js
+++ 0x0D-nf_crypto/nf_crypto/src/index.js
@@ -5,11 +5,16 @@ import App from './App'
 
 import 'antd/dist/antd.css'
 
+import { Provider } from 'react-redux';
+import store from './app/store';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App/>
+      </Provider>
     </Router>
   </React.StrictMode>
 );
