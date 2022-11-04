e93750079d4817af2983f6412bfba8147e00cc81
brk-a <fnjakai@outlook.com>
2022-11-04 03:57:46 +0000

    layout of app

--- 0x0D-nf_crypto/nf_crypto/src/App.js
+++ 0x0D-nf_crypto/nf_crypto/src/App.js
@@ -1,8 +1,8 @@
 import React from 'react'
-import { Switch, Route, Link } from 'react-router-dom'
+import { Routes, Route, Link } from 'react-router-dom'
 import { Layout, Typography, Space } from 'antd'
 
-import { Navbar } from './components'
+import { Navbar, Cryptocurrencies, CryptoDetails, Homepage, News, Exchanges } from './components'
 
 import './App.css'
 
@@ -13,7 +13,20 @@ const App = () => {
           <Navbar/>
         </div>
 
-        <div className='main'></div>
+        <div className='main'>
+          <Layout>
+            <div className='routes'>
+              <Routes>
+                <Route exact path='/' element={<Homepage/>}></Route>
+                <Route exact path='/exchanges' element={<Exchanges/>}></Route>
+                <Route exact path='/cryptocurrencies' element={<Cryptocurrencies/>}></Route>
+                <Route exact path='/cryptocurrencies/:coinId' element={<CryptoDetails/>}></Route>
+                <Route exact path='/news' element={<News/>}></Route>
+              </Routes>
+            </div>
+          </Layout>
+        </div>
+
         <div className='footer'></div>
     </div>
   )
