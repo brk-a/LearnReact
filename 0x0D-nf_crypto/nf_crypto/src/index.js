402e5038f44ce41159a97a8bc994462b2029c8de
brk-a <fnjakai@outlook.com>
2022-11-04 12:40:40 +0000

    rapidAPI does not fetch. no error thrown.

--- 0x0D-nf_crypto/nf_crypto/src/index.js
+++ 0x0D-nf_crypto/nf_crypto/src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import App from './App'
 
 import 'antd/dist/antd.css'
+import "antd/dist/antd.min.css";
 
 import { Provider } from 'react-redux';
 import store from './app/store';
--- 0x0D-nf_crypto/nf_crypto/src/services/cryptoApi.js
+++ 0x0D-nf_crypto/nf_crypto/src/services/cryptoApi.js
@@ -1,18 +1,23 @@
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query'
+import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react'
 
 const cryptoApiHeaders = {
-    'X-rapidapi-host': process.env.REACT_RAPID_API_HOST,
-    'X-rapidapi-key': process.env.REACT_RAPID_API_KEY
+    'X-RapidAPI-Host': process.env.REACT_RAPID_API_HOST,
+    'X-RapidAPI-Key': process.env.REACT_RAPID_API_KEY
 }
 
 const baseUrl = process.env.REACT_RAPID_API_BASE_URL
 
 const createRequest = (url) => ({url, headers: cryptoApiHeaders})
+// const createRequest = (url) => url
 
 export const cryptoApi = createApi({
     reducerPath: 'cryptoApi',
     baseQuery: fetchBaseQuery({baseUrl}),
     endpoints: (builder) => ({
-        getCryptos: builder.query({query: () => createRequest('/exchanges')})
+        getCryptos: builder.query({query: () => createRequest('/coins')})
     })
 })
+
+export const {
+    useGetCryptosQuery,
+} = cryptoApi
