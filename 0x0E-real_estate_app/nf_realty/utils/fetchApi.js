809be828b34e1b1b8be37585e82cc6e7acbd9c63
brk-a <fnjakai@outlook.com>
2022-11-09 10:48:36 +0000

    add params to API call: error  401 is gone and error 500 is in :(

--- 0x0E-real_estate_app/nf_realty/utils/fetchApi.js
+++ 0x0E-real_estate_app/nf_realty/utils/fetchApi.js
@@ -4,6 +4,10 @@ export const baseUrl = process.env.REACT_RAPID_API_URL
 
 export const fetchApi = async (url) => {
     const {data} = await axios.get((url), {
+        params: {
+            agencySlug: 'patriot-real-estate-7737',
+            sort: 'price-asc',
+        },
         headers: {
             'X-RapidAPI-Key': process.env.REACT_RAPID_API_KEY,
             'X-RapidAPI-Host': process.env.REACT_RAPID_API_HOST 
