419f6ba3dbe06a6a1664631e48b530cceabb584b
brk-a <fnjakai@outlook.com>
2022-11-13 15:51:48 +0000

    Search page: part 2

--- 0x0E-real_estate_app/nf_realty/pages/Search.tsx
+++ 0x0E-real_estate_app/nf_realty/pages/Search.tsx
@@ -4,8 +4,10 @@ import Image from "next/image";
 import {Flex, Box, Text, Icon} from '@chakra-ui/react'
 import { BsFilter } from "react-icons/bs";
 import SearchFilters from "../components/SearchFilters";
+import Property from "../components/Property";
+import noresult from '../assets/images/noresult.png'
 
-const Search = () => {
+const Search = ({properties}: any) => {
     const [searchFilters, setSearchFilters] = useState(false)
     const router = useRouter()
 
@@ -26,12 +28,37 @@ const Search = () => {
                 justifyContent='center'
                 alignItems={`center`}
                 onClick={handleClickFilter}>
-                    <Text></Text>
+                    <Text>Search</Text>
                     <Icon paddingLeft={`2`} w='7' as={BsFilter}/>
             </Flex>
             {searchFilters && <SearchFilters/>}
+            <Text fontSize={`2xl`} p='4' fontWeight={`bold`}>
+                Properties
+                {router.query.purpose}
+            </Text>
+            <Flex flexWrap={`wrap`}>
+                {properties.map((property: any) => (
+                    <Property key={property.id} property={property}></Property>
+                ))}
+            </Flex>
+            {properties.length === 0 && (
+                <Flex justifyContent={`center`} alignItems={`center`} flexDirection='column' marginTop={`5`} marginBottom={`5`}>
+                    <Image alt="no result" src={noresult}/>
+                    <Text fontSize={`2xl`} marginTop='3'>No Results Found</Text>
+                </Flex>
+            )}
         </Box>
     )
 }
 
 export default Search
+
+export async function getStaticProps() {
+    const purpose = {}
+
+    return {
+      props: {
+        propertiesForSale: purpose,
+      }
+    }
+  }
