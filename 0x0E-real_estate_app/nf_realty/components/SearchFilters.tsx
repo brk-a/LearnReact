b19b91a56ac077ad25b59972813bb0dae83039fc
brk-a <fnjakai@outlook.com>
2022-11-16 11:46:19 +0000

    `Search Filters`: done

--- 0x0E-real_estate_app/nf_realty/components/SearchFilters.tsx
+++ 0x0E-real_estate_app/nf_realty/components/SearchFilters.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { useEffect, useState } from 'react'
 import {Flex, Select, Box, Text, Input, Spinner, Icon, Button, filter} from '@chakra-ui/react'
-import { useRouter } from 'next/router'
+import router, { useRouter } from 'next/router'
 import {MdCancel} from 'react-icons/md'
 import Image from 'next/image'
 
@@ -10,19 +10,28 @@ import {filterData, getFilterValues} from '../utils/filterData'
 const SearchFilters = () => {
   const [filters, setFilters] = useState(filterData)
   
-  const searchProperties = (filterValues: any) => (
-    ''
-  )
+  const searchProperties = (filterValues: any) => {
+    const path = router.pathname
+    const {query} = router
 
-  const handleChangeFilter = (e: any) => (
-    searchProperties({[filter.queryName]: e.target.value})
-  )
+    const values = getFilterValues(filterValues)
+
+    values.forEach((item) => {
+      query[item.name] = item.value
+    })
+
+    router.push({pathname: path, query})
+  }
+
+  // const handleChangeFilter = (e: any, filter: any) => (
+  //   searchProperties({[filter.queryName]: e.target.value})
+  // )
 
   return (
     <Flex bg={`gray.100`} p='4' justifyContent={`center`} flexWrap='wrap'>
       {filters.map((filter) => (
         <Box key={filter.queryName}>
-          <Select onChange={handleChangeFilter} placeholder={filter.placeholder} w='fit-content' p='2'>
+          <Select onChange={(e) => searchProperties({[filter.queryName]: e.target.value})} placeholder={filter.placeholder} w='fit-content' p='2'>
             {filter?.items?.map((item) => (
               <option value={item.value} key={item.value}>
                 {item.name}
