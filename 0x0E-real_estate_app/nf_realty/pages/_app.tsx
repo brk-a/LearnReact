b2a49158968d6e72f5c3880e4c8eea6152856dcc
brk-a <fnjakai@outlook.com>
2022-11-21 12:07:26 +0000

    `NProgress`: user can see load progress

--- 0x0E-real_estate_app/nf_realty/pages/_app.tsx
+++ 0x0E-real_estate_app/nf_realty/pages/_app.tsx
@@ -7,6 +7,16 @@ import NProgress from 'nprogress'
 import Layout from '../components/Layout'
 
 export default function App({ Component, pageProps }: AppProps) {
+  NProgress.configure({showSpinner: false})
+
+  Router.events.on('routeChangeStart', () => {
+    NProgress.start()
+  })
+
+  Router.events.on('routeChangeComplete', () => {
+    NProgress.done()
+  })
+
   return(
     <>
       <Head>
