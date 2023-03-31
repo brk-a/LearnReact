7e9353df804e746ef5d270876167ef0a2af5e93a
brk-a <fnjakai@outlook.com>
2023-03-31 23:17:22 +0000

    `mvc_crud_app`: initial commit

---
+++ 0x0F-mvc_crud/mvc_crud_app/src/components/StudentTableRow.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const StudentTableRow = () => {
+  return (
+    <div>StudentTableRow</div>
+  )
+}
+
+export default StudentTableRow
---
+++ 0x0F-mvc_crud/mvc_crud_app/src/components/index.js
@@ -0,0 +1,13 @@
+import CreateStudent from "./CreateStudent";
+import EditStudent from "./EditStudent";
+import StudentForm from "./StudentForm";
+import StudentList from "./StudentList";
+import StudentTableRow from "./StudentTableRow";
+
+export{
+    CreateStudent,
+    EditStudent,
+    StudentForm,
+    StudentList,
+    StudentTableRow
+}
